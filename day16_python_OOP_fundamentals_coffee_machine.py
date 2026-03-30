e31ff392d836a1752fa63532c6be7e586e400604
Jasing Pathiranage Omath Dintharu <omathdintharu0@gmail.com>
2026-03-30 19:45:15 +0530

    Add files via upload

---
+++ day16_python_OOP_fundamentals_coffee_machine.py
@@ -0,0 +1,22 @@
+class CoffeeMachine:
+    def __init__(self, brand, water_level):
+        # Attributes (දත්ත)
+        self.brand = brand
+        self.water_level = water_level
+        print(f"--- {self.brand} Coffee Machine is ready !")
+
+
+    def make_coffee(self, cup_size):
+        if self.water_level >= cup_size:
+            self.water_level -= cup_size
+            print(f"Here u are ur coffee... Enjoy ! (Remaining water:{self.water_level}ml)")
+        else:
+            print("Not enough water, buddy! Fill it again.")
+
+
+my_machine = CoffeeMachine("Omath's Special", 500)
+
+# 3. මැෂින් එක පාවිච්චි කරමු
+my_machine.make_coffee(150)
+my_machine.make_coffee(200)
+my_machine.make_coffee(200)
