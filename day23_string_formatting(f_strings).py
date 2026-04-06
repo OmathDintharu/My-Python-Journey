044c660294e0e984ec7edc53d59907e57b937781
Jasing Pathiranage Omath Dintharu <omathdintharu0@gmail.com>
2026-04-06 21:37:19 +0530

    Add files via upload

---
+++ day23_string_formatting(f_strings).py
@@ -0,0 +1,10 @@
+# Day 23: Learning F-Strings for Professional Output
+name = "Omath"
+mission = "Global Secure-Tech Hub 2034"
+
+# Using f-string to combine variables and text
+status_report = f"User: {name} | Mission: {mission} | Status: Neural Adaptation in Progress"
+
+print(status_report)
+
+input("\n Press Enter to exit...")
