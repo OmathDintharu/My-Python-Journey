ee45e5e7d446bc23d4017755f0ff7b96de74b525
Jasing Pathiranage Omath Dintharu <omathdintharu0@gmail.com>
2026-04-18 23:11:15 +0530

    Add files via upload

---
+++ day33_fstrings.py
@@ -0,0 +1,13 @@
+# Python Day 32 - Minimalist String Formatting
+# Language: Python
+
+name = "Omath"
+level = "N5"
+status = "Learning"
+
+# Using f-string for a clean output
+message = f"User: {name} | Goal: {level} Mastery | Status: {status}"
+
+print("--- Mission 2034 Progress ---")
+print(message)
+print("-----------------------------")
