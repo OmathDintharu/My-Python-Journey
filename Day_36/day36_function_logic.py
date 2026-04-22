a1fd339edbd275913a0f61dacc1df75b0702fa39
Jasing Pathiranage Omath Dintharu <omathdintharu0@gmail.com>
2026-04-22 22:57:27 +0530

    Add Day 36: Simple Function Code
    -Add a simple code about my dream thing.

---
+++ Day_36/day36_function_logic.py
@@ -0,0 +1,9 @@
+# Function 
+def subaru_music_logic(song_name, db_level):
+    print("--- S-Logic System Active ---")
+    print("Processing Song: " + song_name)
+    print("Target dB Balance: " + str(db_level) + "dB")
+    print("Status: 8D Ready & Bass Boosted! 🚀")
+
+# Function(Call)
+subaru_music_logic("They Don't Know About Us", -6)
